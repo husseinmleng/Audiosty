3ca06f9d5714092bc9f725d5a3230ff2e022c8ed
elsebaiy elsebaiy192156 <elsebaiy192156@fci.bu.edu.eg>
2023-12-24 22:07:17 +0200

    format file

--- app.py
+++ app.py
@@ -8,33 +8,36 @@ CORS(app)
 
 @app.route('/analyze_audio', methods=['POST', 'GET'])
 def analyze_audio():
-  if 'audio' not in request.files:
-        return "No audio part", 400
-  if 'text' not in request.files:
-    return "No text part", 400
-
-  audio = request.files['audio']
-  text = request.files['text']
-  if text.filename == '' or audio.filename == '':
+    if 'audio' not in request.files:
+        return "audio is missing", 400
+    if 'text' not in request.files:
+        return "text is missing", 400
+    
+    audio = request.files['audio']
+    text = request.files['text']
+    if text.filename == '' or audio.filename == '':
         return "No selected file", 400
-  if audio and text:
+    
+    if audio and text:
         # You can add file saving logic here
-    temp_filename = 'temp_audio.wav'
-    wavfile.write(temp_filename, audio[0], audio[1])
+        temp_filename = 'temp_audio.wav'
+        wavfile.write(temp_filename, audio[0], audio[1])
 
 
-    result = Speaker_speech_analysis(temp_filename, text)
-    accuracy_score = result['pronunciation_accuracy']
-    fluency_score  = result['fluency_score']
-    word_scores    = result['word_scores']
-    response = {
-            "Accuracy": accuracy_score,
-            "Fluency": fluency_score,
-            'Words_score': word_scores
-        }
-    return jsonify(response), 200
+        result = Speaker_speech_analysis(temp_filename, text)
+        accuracy_score = result['pronunciation_accuracy']
+        fluency_score  = result['fluency_score']
+        word_scores    = result['word_scores']
+        response = {
+                "Accuracy": accuracy_score,
+                "Fluency": fluency_score,
+                'Words_score': word_scores
+            }
+        return jsonify(response), 200
 
-  return "Error processing request", 400
+    return "Error processing request", 400
 
 if __name__ == '__main__':
     app.run(debug=False, host='0.0.0.0', port=5000)
+    
+    
