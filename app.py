5dab47f1b1700c3a6fd6eb68161d07cbf0ffa03a
elsebaiy elsebaiy192156 <elsebaiy192156@fci.bu.edu.eg>
2023-12-26 14:06:48 +0200

    add Procfile

--- app.py
+++ app.py
@@ -38,6 +38,6 @@ def analyze_audio():
     return "Error processing request", 400
 
 if __name__ == '__main__':
-    app.run(debug=False, host='0.0.0.0', port=5000)
+    app.run(debug=False)
     
     
